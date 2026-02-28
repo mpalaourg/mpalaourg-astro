232c89d8ade70d0d398e2804ceae829707cebaa0
Georgios Balaouras <gbalaouras@yahoo.com>
2026-02-28 17:56:57 +0200

    add publications and formula page

--- src/content/config.ts
+++ src/content/config.ts
@@ -33,7 +33,59 @@ const formulaEl = defineCollection({
   }),
 });
 
+const publications = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    authors: z.array(z.string()),
+    author_notes: z.array(z.string()).optional(),
+    date: z.date(),
+    doi: z.string().optional(),
+    publication_types: z.array(z.string()),
+    publication: z.string(),
+    publication_short: z.string(),
+    abstract: z.string(),
+    summary: z.string(),
+    tags: z.array(z.string()).optional(),
+    featured: z.boolean().optional(),
+    url_pdf: z.string().optional(),
+    url_code: z.string().optional(),
+    url_dataset: z.string().optional(),
+    url_slides: z.string().optional(),
+    url_video: z.string().optional(),
+    url_project: z.string().optional(),
+    featuredImage: z.string().optional(),
+  }),
+});
+
+const publicationsEl = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    authors: z.array(z.string()),
+    author_notes: z.array(z.string()).optional(),
+    date: z.date(),
+    doi: z.string().optional(),
+    publication_types: z.array(z.string()),
+    publication: z.string(),
+    publication_short: z.string(),
+    abstract: z.string(),
+    summary: z.string(),
+    tags: z.array(z.string()).optional(),
+    featured: z.boolean().optional(),
+    url_pdf: z.string().optional(),
+    url_code: z.string().optional(),
+    url_dataset: z.string().optional(),
+    url_slides: z.string().optional(),
+    url_video: z.string().optional(),
+    url_project: z.string().optional(),
+    featuredImage: z.string().optional(),
+  }),
+});
+
 export const collections = {
   formula,
   formulaEl,
+  publications,
+  publicationsEl,
 };
