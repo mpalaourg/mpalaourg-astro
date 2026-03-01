20901939e02d93b8fde32e94e640274a60b2af20
Georgios Balaouras <gbalaouras@yahoo.com>
2026-03-01 20:53:29 +0200

    fix config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import mdx from "@astrojs/mdx";
 
 export default defineConfig({
+  output: 'static',
   integrations: [mdx()],
   vite: {
     plugins: [tailwindcss()],
